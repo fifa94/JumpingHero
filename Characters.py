16c7b988793029e50b3bb5802793db736e5ddd58
Pavel Houfek <pavel.houfek@br-automation.com>
2023-01-19 14:28:51 +0100

    enhanc(Character): add basic methods

--- Characters.py
+++ Characters.py
@@ -3,13 +3,50 @@ class commonCharacter:
     self.name = name
     self.hp = 100
     self.stamina = 100
+    self.perk = 'none'
 
   def __str__(self):
-    return self.name
+    new_line = '\n'
+    separator = '====================================================' + new_line
+    text_name = 'character name: ' + str(self.getName()) + new_line
+    text_hp = 'hit points: ' + str(self.getHP()) + new_line
+    text_stamina = 'stamina: ' + str(self.getStamina()) + new_line
+    output_text = separator + text_name + text_hp + text_stamina + separator
+    return output_text
+
+  def move_right(self):
+    pass
+
+  def move_left(self):
+    pass
+
+  def jump(self):
+    pass
+
+  def gravity(self):
+    pass
+  def increase_hitpoint(self):
+    pass
+
+  def decrease_hitpoint(self):
+    pass
+
+  def increase_stamina(self):
+    pass
+
+  def decrease_stamina(self):
+    pass
+
+  def getHP(self): return self.hp
+
+  def getStamina(self): return self.stamina
+
+  def getName(self): return self.name
 
 
-
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
 
 a = commonCharacter('Franta')
-
 print(a)
