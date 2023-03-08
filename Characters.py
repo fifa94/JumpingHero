d3da80a83934a42e2ea457470f9c8e1bf357d256
Pavel Houfek <pavel.houfek@br-automation.com>
2023-03-08 13:36:31 +0100

    fix(Object): Move right

--- Characters.py
+++ Characters.py
@@ -13,6 +13,8 @@ class CommonCharacter:
         self.player = pygame.image.load('Pictures/rocket.png')
         self.draw()
 
+        self.rotation_flag = True
+
     def __str__(self):
         new_line = '\n'
         separator = '====================================================' + new_line
@@ -23,23 +25,34 @@ class CommonCharacter:
         return output_text
 
     def draw(self):
+
         self.screen.blit(self.player, (self.pos_x, self.pos_y))
 
 
     def move_right(self):
-      print('move right')
-      self.pos_x += 5
-      self.draw()
+        print('move right')
+        self.pos_x += 5
+        if self.rotation_flag:
+            self.player = pygame.transform.rotate(self.player, 45)
+            self.rotation_flag = False
+
+
+
+        self.draw()
 
 
     def move_left(self):
       print('move left')
       self.pos_x -= 5
+      if self.rotation_flag:
+          self.player = pygame.transform.rotate(self.player, 135)
+          self.rotation_flag = False
       self.draw()
 
     def move_up(self):
       print('move up')
       self.pos_y -= 5
+      self.rotation_flag = True
       self.draw()
 
     def move_down(self):
